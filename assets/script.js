0ad39d41509c95141635e8039af91d538285bd33
Quelz <marqueldavis@marquels-mbp.mynetworksettings.com>
2024-07-29 20:55:22 -0400

    updatedwebpage

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,31 @@
 
-document.getElementById('btn').addEventListener('click', function () {
+
+
+document.getElementById('btn').addEventListener('click', function addToLocalStorage() {
+    let quantity = document.getElementById('QuantityInput').value;
     let productDetails = {
         productName: 'EASYBIKE2',
-        productPrice: '$50',
-        productDesc: 'The EasyBike2 is a great bike for beginners and experienced riders alike. It is lightweight and easy to handle, making it perfect for commuting or leisurely rides around town. The EasyBike2 features a durable steel frame and fork, 26-inch wheels, and a 7-speed Shimano drivetrain. It also has front and rear linear pull brakes for reliable stopping power. Whether you are looking for a reliable commuter bike or a fun way to get around town, the EasyBike2 is a great choice.',
+        productPrice: '$200',
+        productDesc:"The EASY BIKE2.0 is a great bike for beginners and experienced riders alike. It is lightweight and easy to handle, making it perfect for commuting or enjoying rides around town. The EASY BIKE2.0 features a durable steel frame and fork, and 26-inch wheels. It also has front and rear linear pull brakes for reliable stopping power. Whether you are looking for a reliable commuter bike or a fun way to get around town, the EASY BIKE2.0 is a great choice.",
+        productQuantity: quantity
+    
+    };
+
+    if (productDetails.productQuantity > 50) {
+        alert('Please enter a quantity number lower than 50');
+    } else {
+        localStorage.setItem('productDetails', JSON.stringify(productDetails));
+    }
+    if (productDetails.productQuantity < 1) {
+        alert('Please enter a quantity number higher than 0');
     }
 
-    localStorage.setItem('productDetails', JSON.stringify(productDetails));
-    // window.location.href = 'checkout.html';
+
+
+    // localStorage.setItem('productDetails', JSON.stringify(productDetails));
 
 });
 
-
-// productDetails = [{ 
-//     productName: 'EASYBIKE2',
-//     productPrice: '$50',
-//     productDesc: 'The EasyBike2 is a great bike for beginners and experienced riders alike. It is lightweight and easy to handle, making it perfect for commuting or leisurely rides around town. The EasyBike2 features a durable steel frame and fork, 26-inch wheels, and a 7-speed Shimano drivetrain. It also has front and rear linear pull brakes for reliable stopping power. Whether you are looking for a reliable commuter bike or a fun way to get around town, the EasyBike2 is a great choice.',
-// },{
-//     productName: 'EASYBIKE3',
-//     productPrice: '$60',
-//     productDesc: 'The EasyBike2 is a great bike for beginners and experienced riders alike. It is lightweight and easy to handle, making it perfect for commuting or leisurely rides around town. The EasyBike2 features a durable steel frame and fork, 26-inch wheels, and a 7-speed Shimano drivetrain. It also has front and rear linear pull brakes for reliable stopping power. Whether you are looking for a reliable commuter bike or a fun way to get around town, the EasyBike2 is a great choice.',
 
 
 
